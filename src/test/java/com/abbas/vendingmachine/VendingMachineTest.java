9605cafa381fea341b805592144ba76723907684
Abbas.d67@gmail.com <Abbas.d67@gmail.com>
2022-01-22 18:35:21 +0330

    testBuy added

--- src/test/java/com/abbas/vendingmachine/VendingMachineTest.java
+++ src/test/java/com/abbas/vendingmachine/VendingMachineTest.java
@@ -1,8 +1,8 @@
 package com.abbas.vendingmachine;
 
 import com.abbas.vendingmachine.entities.Enums;
-import com.abbas.vendingmachine.models.LoginModel;
-import com.abbas.vendingmachine.models.UserModel;
+import com.abbas.vendingmachine.entities.Product;
+import com.abbas.vendingmachine.models.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
@@ -49,7 +52,8 @@ public class VendingMachineTest extends DemoApplicationTests {
                         .post("/login")
                         .content(asJsonString(new LoginModel("Abbas", "Dehghan")))
                         .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON)
+                )
                 .andDo(print())
                 .andExpect(status().isOk());
 
@@ -67,6 +71,32 @@ public class VendingMachineTest extends DemoApplicationTests {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("20")));
+
+    }
+
+    @Test
+    public void testBuy() throws Exception {
+
+        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJCdXllcjIiLCJleHAiOjE2NDI4ODA1ODYsImlhdCI6MTY0Mjg2MjU4Nn0.t2njc9z7G7sfrjPR8JK3pCRyosOayF8-v1xCQD9ZZ_FzvQRvMdLqgxxaDR5eOYbbk7ogo2SbSnfw1EkGIzxggA";
+
+        BuyProductModel buy1 = new BuyProductModel(1, 2);
+        BuyProductModel buy2 = new BuyProductModel(2, 3);
+        BuyModel model = new BuyModel(List.of(buy1, buy2));
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                        .post("/buy")
+                        .header("Authorization", "Bearer " + token)
+                        .content(asJsonString(model))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.products").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalSpent").value(65))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.products[*].amountAvailable").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.products[0].amountAvailable").value(98))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.products[1].amountAvailable").value(7));
 
     }
 
